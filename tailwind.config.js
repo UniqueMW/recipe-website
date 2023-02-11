de3b5e0367f9012f2be852c1ce4cae11fd7b65c8
Cliff Likovo <clifflikovo@gmail.com>
2023-02-11 02:53:04 +0200

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: '#FFFFFC',
+      secondary: '#072AC8',
+      action: '#C9184A'
+    },
+    extend: {}
+  },
+
+  plugins: []
+}
