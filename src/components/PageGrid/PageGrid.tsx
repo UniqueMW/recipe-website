f4b3b1939e8b339178a0209c8057540447eead3f
Cliff Likovo <clifflikovo@gmail.com>
2023-02-24 00:30:49 +0200

    added page grid

---
+++ src/components/PageGrid/PageGrid.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import ErrorFallback from 'components/ErrorFallback/ErrorFallback'
+import Card from 'components/Card/Card'
+import { useFetch } from 'hooks'
+import type { CardMeals } from 'types'
+
+interface IPageGridProps {
+  url: string
+}
+
+function PageGrid(props: IPageGridProps): JSX.Element {
+  const fetchedData = useFetch<CardMeals>(props.url)
+
+  if (typeof fetchedData !== 'undefined') {
+    // takes the first specified amount from fetched data array.
+    // generate an array of cards.
+    const cards = fetchedData.meals.map((meal) => (
+      <ErrorBoundary FallbackComponent={ErrorFallback} key={meal.idMeal}>
+        <Card
+          meal={meal.strMeal}
+          img={meal.strMealThumb}
+          mealId={meal.idMeal}
+        />
+      </ErrorBoundary>
+    ))
+
+    // display the whole grid as required.
+    return (
+      <section className="flex flex-col md:px-10 px-2 space-y-4 mb-4 text-base md:text-xl font-sans font-normal md:font-normal tracking-wide">
+        <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 grid-flow-row gap-4">
+          {cards}
+        </section>
+      </section>
+    )
+  } else {
+    // handle the loading state of the component.
+    return <h1>Loading....</h1>
+  }
+}
+
+export default PageGrid
