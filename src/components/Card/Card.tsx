0b955e02f61ffa80047dae0e6668691a9e9815d7
Cliff Likovo <clifflikovo@gmail.com>
2023-02-25 20:09:53 +0200

    finished filter component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,9 +8,9 @@ interface CardProps {
 
 function Card(props: CardProps): JSX.Element {
   return (
-    <section className="flex flex-col max-w-fit space-y-3 border border-gray-500">
+    <section className="flex flex-col max-w-fit space-y-3 border border-gray-500 min-w-full ">
       <img src={props.img} alt={props.meal} />
-      <h2 className="text-lg font-medium font-sans">{props.meal}</h2>
+      <h2 className="text-lg font-medium font-sans px-1">{props.meal}</h2>
     </section>
   )
 }
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,7 @@
-import { useFetch } from 'hooks'
 import * as React from 'react'
-import { BiFilterAlt } from 'react-icons/bi'
+import { IoFilterSharp } from 'react-icons/io5'
 import _ from 'lodash'
+import { useFetch } from 'hooks'
 
 import type { CategoryObject, LocationObject, IngredientObject } from 'types'
 
@@ -18,32 +18,32 @@ function Filter(props: IFilterProps): JSX.Element {
   // fetch filter options value based on passed url
   const fetchedData = useFetch<GridInfoObjects>(props.url)
 
-  const entries: string[] = []
+  const [entries, setEntries] = React.useState<string[]>()
 
-  // get all the objects values and add them to entries array
-  fetchedData?.meals.map((item) => {
-    _.forIn(item, (content, key) => {
-      if (key === props.valueKey) {
-        entries.push(content)
-      }
+  React.useEffect(() => {
+    // get all the objects values and add them to entries array
+    const filterArray: string[] = []
+
+    fetchedData?.meals.map((item) => {
+      _.forIn(item, (content, key) => {
+        if (key === props.valueKey) {
+          filterArray.push(content)
+        }
+      })
+      return null
     })
-    return null
-  })
+
+    setEntries(filterArray)
+  }, [fetchedData])
 
   // creating option element to be passed to the select element, by iterating entries.
   // Selected option is created based on the initial value
   const optionsArray = React.useMemo(() => {
-    return entries?.map((item: string, index) =>
-      item !== props.initial ? (
-        <option key={`${item}${index}`} value={item}>
-          {item}
-        </option>
-      ) : (
-        <option key={`${item}${index}`} value={item} selected>
-          {item}
-        </option>
-      )
-    )
+    return entries?.map((item: string, index) => (
+      <option key={`${item}${index}`} value={item}>
+        {item}
+      </option>
+    ))
   }, [entries])
 
   const handleSelect = (event: React.FormEvent<HTMLSelectElement>): void => {
@@ -54,16 +54,20 @@ function Filter(props: IFilterProps): JSX.Element {
   if (typeof optionsArray !== 'undefined') {
     return (
       <div className="px-2 md:px-12 mb-4 md:mb-6">
-        <section className="flex flex-row justify-between items-center pt-20 border-b-2 border-gray-500">
-          <div className="flex flex-row max-w-fit items-center md:text-2xl text-xl font-sans font-semibold tracking-wider">
-            <BiFilterAlt />
-            <h2>Filter</h2>
+        <form className="flex flex-row justify-between items-center pt-20">
+          <div className="flex flex-row max-w-fit items-center md:text-2xl text-xl font-sans font-semibold tracking-wider space-x-2">
+            <h2>Catalog</h2>
+            <IoFilterSharp />
           </div>
 
-          <select className="outline-none min-w-[15%]" onChange={handleSelect}>
+          <select
+            className="outline-none min-w-[15%] border-2 border-gray-500 p-2"
+            onChange={handleSelect}
+            defaultValue={props.initial}
+          >
             {optionsArray}
           </select>
-        </section>
+        </form>
       </div>
     )
   }
