9e671040e8c39c6506dbf946c896388b9b055b08
Cliff Likovo <clifflikovo@gmail.com>
2023-02-25 22:30:31 +0200

    created meal page

---
+++ src/pages/MealPage/MealPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MealDetail } from 'components'
+
+function MealPage(): JSX.Element {
+  return (
+    <>
+      <MealDetail />
+    </>
+  )
+}
+
+export default MealPage
