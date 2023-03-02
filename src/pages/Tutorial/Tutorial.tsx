c23e6c11e7850100f56add3d3c4d73ce5b6ae306
Cliff Likovo <clifflikovo@gmail.com>
2023-03-03 00:30:17 +0200

    added youtube iframe to display tutorial

--- src/pages/Tutorial/Tutorial.tsx
+++ src/pages/Tutorial/Tutorial.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react'
 import { useParams } from 'react-router-dom'
+import _ from 'lodash'
 
 function Tutorial(): JSX.Element {
   const { videoId } = useParams()
-  return <h1 className="text-6xl mt-96">{videoId}</h1>
+  return (
+    <section className="flex flex-col justify-center items-center md:px-12 px-2 pt-16 md:pt-20">
+      <iframe
+        className="min-w-full min-h-[80vh]"
+        src={`https://www.youtube.com/embed/${_.trim(
+          videoId,
+          ':'
+        )}?autoplay=1&fs=1`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+    </section>
+  )
 }
 
 export default Tutorial
