26d6abc50e2eb059b556e3d03ff15cd0e5c0468a
Cliff Likovo <clifflikovo@gmail.com>
2023-03-03 17:42:28 +0200

    improved mobile nav

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -11,7 +11,7 @@ function MobileNav(props: MobileNavProps): JSX.Element {
   }
 
   return (
-    <nav className="flex flex-row justify-between content-center py-4 px-2 shadow-md font-sans text-lg md:hidden fixed bg-primary min-w-full z-10">
+    <nav className="flex flex-row justify-between content-center py-4 px-2 shadow-md font-sans text-lg md:hidden fixed bg-primary min-w-full z-10 top-0 left-0">
       <button className="text-lg" onClick={handleDisplayMenu}>
         <GiHamburgerMenu />
       </button>
