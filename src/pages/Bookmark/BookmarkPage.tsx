e5ea2bc14703c687bee375a7822ec7e1c09aca4d
Cliff Likovo <clifflikovo@gmail.com>
2023-03-04 23:01:16 +0200

    improved bookmark page

--- src/pages/Bookmark/BookmarkPage.tsx
+++ src/pages/Bookmark/BookmarkPage.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import { Grid } from 'components'
 import type { CardMeal } from 'types'
-
+// TODO handle empty bookmark
 function BookmarkPage(): JSX.Element {
   const [bookmarkedMeals, setBookmarkedMeal] = React.useState<CardMeal[]>()
+
   React.useEffect(() => {
     const meals = localStorage.getItem('uniqueMW_recipe_bookmark')
     if (typeof meals === 'string') {
       setBookmarkedMeal(JSON.parse(meals))
     }
   }, [])
+
   if (typeof bookmarkedMeals !== 'undefined' && bookmarkedMeals.length > 0) {
     return (
       <section className="py-10">
