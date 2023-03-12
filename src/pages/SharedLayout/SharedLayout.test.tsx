218eb26490399734740be8cba3798febbefa9933
Cliff Likovo <clifflikovo@gmail.com>
2023-03-12 16:51:52 +0200

    added SharedLayout test

---
+++ src/pages/SharedLayout/SharedLayout.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import SharedLayout from './SharedLayout'
+
+// mock Outlet from react-router dom
+jest.mock('react-router-dom', () => ({ Outlet: () => <h1>Outlet</h1> }))
+
+// mock components
+jest.mock('components', () => ({
+  Nav: () => <h1>nav pc</h1>,
+  Footer: () => <h1>Footer</h1>,
+  MobileNav: () => <h1>Mobile nav</h1>,
+  SideMenu: () => <h1>Side Menu</h1>
+}))
+
+// mock gsap library
+jest.mock('gsap')
+
+it('Should render all the components.', () => {
+  render(<SharedLayout />)
+
+  const navComponent = screen.getByRole('heading', { name: /nav pc/i })
+  const footerComponent = screen.getByRole('heading', { name: /footer/i })
+  const mobileNavComponent = screen.getByRole('heading', {
+    name: /mobile nav/i
+  })
+  const sideMenuComponent = screen.getByRole('heading', { name: /side menu/i })
+  const outlet = screen.getByRole('heading', { name: /outlet/i })
+
+  expect(navComponent).toBeInTheDocument()
+  expect(footerComponent).toBeInTheDocument()
+  expect(mobileNavComponent).toBeInTheDocument()
+  expect(sideMenuComponent).toBeInTheDocument()
+  expect(outlet).toBeInTheDocument()
+})
