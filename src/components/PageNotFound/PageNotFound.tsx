fb4750d0e0d88d3146ff5025f3de6912bf3a9cd0
Cliff Likovo <clifflikovo@gmail.com>
2023-03-13 00:12:21 +0200

    improved PageNotFound

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -11,8 +11,12 @@ function PageNotFound(): JSX.Element {
 
   return (
     <section className="md:px-12 px-2 flex flex-col justify-center items-center py-20 space-y-6">
-      <img src={Image404} alt="Page not found" className="max-w-sm" />
-      <h1 className="md:text-2xl text-lg tracking-wider font-sans font-semibold">
+      <img
+        src={Image404}
+        alt="Page not found"
+        className="md:max-w-sm max-w-[15rem]"
+      />
+      <h1 className="md:text-2xl text-lg tracking-wider font-sans font-semibold text-center">
         We couldn&#39;t find the page you were looking for....
       </h1>
       <button
