c8073603c29d917655a1222d8790cb421149315d
Cliff Likovo <clifflikovo@gmail.com>
2023-03-12 10:42:14 +0200

    added category page test

---
+++ src/pages/Category/Category.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Category from './Category'
+
+jest.mock('components', () => ({
+  PageGrid: () => <h1>Page Grid</h1>,
+  Filter: () => <h1>Filter</h1>
+}))
+
+it('Should render PageGrid and Filter Components', () => {
+  render(<Category />)
+
+  const pageGrid = screen.getByRole('heading', { name: /page grid/i })
+  const filter = screen.getByRole('heading', { name: /filter/i })
+
+  expect(pageGrid).toBeInTheDocument()
+  expect(filter).toBeInTheDocument()
+})
