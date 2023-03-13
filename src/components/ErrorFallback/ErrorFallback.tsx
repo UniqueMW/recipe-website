f2e526ff30255f98d95526509fe797a46af70012
Cliff Likovo <clifflikovo@gmail.com>
2023-03-13 10:52:12 +0200

    improved ErrorFallback component

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
 import { IoMdRefresh } from 'react-icons/io'
-import errorImage from 'assets/error.svg'
+import { GoReport } from 'react-icons/go'
+import { useNavigate } from 'react-router-dom'
+import errorImage from 'assets/fatalerror.svg'
 import type { FallbackProps } from 'react-error-boundary'
 
 function ErrorFallback({ resetErrorBoundary }: FallbackProps): JSX.Element {
+  const navigate = useNavigate()
+
+  const handleReport = (): void => {
+    navigate('contact')
+  }
+  const handleTryAgain = (): void => {
+    resetErrorBoundary()
+  }
+
   return (
     <section className=" space-y-4 px-2 md:px-12 mt-4">
-      <section className="flex flex-col items-center justify-between py-4 border">
-        <img
-          src={errorImage}
-          alt="Error"
-          className="max-w-[40%] md:max-w-[20%]"
-        />
-        <h1 className="lg:text-xl text-base text-red-700 font-sans font-semibold text-center">
-          Ooops Something Went Wrong. Please click refresh.
+      <section className="flex flex-col items-center justify-between py-8 space-y-4 border">
+        <img src={errorImage} alt="Error" className="md:max-w-xl" />
+        <h1 className="lg:text-xl text-base font-sans tracking-wider text-center md:max-w-2xl">
+          Something Went Wrong, Make sure you have an internet connection or
+          click try again. If the error continues please report it.
         </h1>
-        <button
-          onClick={() => {
-            resetErrorBoundary()
-          }}
-        >
-          <IoMdRefresh className="lg:text-6xl text-3xl font-sans font-semibold text-action" />
-        </button>
+
+        <section className=" flex flex-row md:space-x-10 space-x-6">
+          <button
+            onClick={handleTryAgain}
+            className="py-3 2xl:px-16 md:px-8 px-3 bg-action text-base md:text-lg text-primary shadow-md tracking-wider flex flex-row items-center min-w-fit"
+          >
+            <IoMdRefresh className="mr-2" />
+            Try Again
+          </button>
+          <button
+            onClick={handleReport}
+            className="border-2 border-action 2xl:px-16 md:px-8 px-2 shadow-sm py-3 text-base md:text-lg tracking-wider flex flex-row items-center min-w-fit"
+          >
+            <GoReport className="mr-2" />
+            Report
+          </button>
+        </section>
       </section>
     </section>
   )
