3888c71373f88b66e8b1825ebde504aba5b305bc
Cliff Likovo <clifflikovo@gmail.com>
2023-03-15 00:26:40 +0200

    added SEO component responsible for meta tags

---
+++ src/components/SEO/SEO.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+
+interface ISEOProps {
+  title: string | undefined
+  description: string
+}
+
+function SEO({
+  title = 'UniqueMW Recipe',
+  description
+}: ISEOProps): JSX.Element {
+  return (
+    <Helmet>
+      {/* Standard metadata tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      {/* End standard metadata tags */}
+      {/* Facebook tags */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {/* End Facebook tags */}
+      {/* Twitter tags */}
+      <meta name="twitter:creator" content="@UniqueMW265" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      {/* End Twitter tags */}
+    </Helmet>
+  )
+}
+
+export default SEO
