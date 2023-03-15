13cd36eefacfc2b4326631fe7f3c5bf2cf3f08f4
Cliff Likovo <clifflikovo@gmail.com>
2023-03-15 02:50:47 +0200

    changed card grid to return empty

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import _ from 'lodash'
 import { ErrorBoundary } from 'react-error-boundary'
-import { Card, ErrorFallback } from 'components'
+import { Card, Empty, ErrorFallback } from 'components'
 import type { CardMeal } from 'types'
 
 interface GridProps {
@@ -24,20 +24,24 @@ function Grid(props: GridProps): JSX.Element {
   ))
 
   // display the whole grid as required.
-  return (
-    <section className="flex flex-col md:px-10 px-2 space-y-4 mb-4 text-base md:text-xl font-sans font-normal md:font-normal tracking-wide">
-      {props.gridContent.length > 0 ? (
-        <h1 className="mt-10 text-xl md:text-2xl font-sans font-semibold border-b border-b-gray-500">
-          {props.gridContent}
-        </h1>
-      ) : (
-        <h1></h1>
-      )}
-      <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 grid-flow-row gap-4">
-        {cards}
+  if (cards.length > 0) {
+    return (
+      <section className="flex flex-col md:px-10 px-2 space-y-4 mb-4 text-base md:text-xl font-sans font-normal md:font-normal tracking-wide">
+        {props.gridContent.length > 0 ? (
+          <h1 className="mt-10 text-xl md:text-2xl font-sans font-semibold border-b border-b-gray-500">
+            {props.gridContent}
+          </h1>
+        ) : (
+          <h1></h1>
+        )}
+        <section className="grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 grid-flow-row gap-4">
+          {cards}
+        </section>
       </section>
-    </section>
-  )
+    )
+  }
+
+  return <Empty />
 }
 
 export default Grid
